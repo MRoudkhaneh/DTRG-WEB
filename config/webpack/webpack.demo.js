d7bdb78cb6b983561b333ec86a032a0f54f43ab4
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-07-13 21:31:50 +0430

    BUNDLE: Fix routing

--- config/webpack/webpack.demo.js
+++ config/webpack/webpack.demo.js
@@ -9,63 +9,63 @@ const BundleAnalyzerPlugin =
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  //bail: true,
-  // optimization: {
-  //   minimize: true,
-  //   minimizer: [
-  //     new UglifyJsPlugin({
-  //       uglifyOptions: {
-  //         mangle: {
-  //           keep_fnames: true,
-  //         },
-  //       },
-  //     }),
-  //     new TerserPlugin({
-  //       terserOptions: {
-  //         parse: {
-  //           ecma: 8,
-  //         },
-  //         compress: {
-  //           ecma: 5,
-  //           warnings: false,
-  //           comparisons: false,
-  //           inline: 2,
-  //         },
-  //         mangle: {
-  //           safari10: true,
-  //         },
-  //         keep_classnames: true,
-  //         keep_fnames: true,
-  //         output: {
-  //           ecma: 5,
-  //           comments: false,
-  //           ascii_only: true,
-  //         },
-  //       },
-  //     }),
-  //     new CssMinimizerPlugin(),
-  //   ],
-  //   runtimeChunk: 'single',
-  //   moduleIds: 'deterministic',
-  //   usedExports: true,
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     cacheGroups: {
-  //       vendor: {
-  //         test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom|react-datepicker|react-hook-form|react-query)[\\/]/,
-  //         priority: -10,
-  //         reuseExistingChunk: true,
-  //         name: 'vendor',
-  //         chunks: 'all',
-  //       },
-  //       default: {
-  //         minChunks: 2,
-  //         priority: -20,
-  //         reuseExistingChunk: true,
-  //       },
-  //     },
-  //   },
-  // },
+  bail: true,
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      }),
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          keep_classnames: true,
+          keep_fnames: true,
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    runtimeChunk: 'single',
+    moduleIds: 'deterministic',
+    usedExports: true,
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom|react-datepicker|react-hook-form|react-query)[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+          name: 'vendor',
+          chunks: 'all',
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.variable': JSON.stringify('demo'),
--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,19 +1,11 @@
-import { lazy, memo, Suspense } from 'react'
+import { memo, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Toast } from 'components/toast'
-//import { Page } from 'components/page'
+import { Page } from 'components/page'
+import { Skeleton } from 'components/skeleton'
 import { useUi } from 'hooks/use-ui'
 import { AdminLayout } from 'layouts/admin'
-//import { routes } from './routes'
-
-const Home = lazy(() => import('pages/home'))
-const Login = lazy(() => import('pages/authentication/login'))
-const Password = lazy(() => import('pages/authentication/password'))
-const Register = lazy(() => import('pages/authentication/register'))
-const Patients = lazy(() => import('pages/patients/list'))
-const PatientForm = lazy(() => import('pages/patients/add'))
-const PatientInteractions = lazy(() => import('pages/patients/interactions'))
-const PatientAssets = lazy(() => import('pages/patients/assets'))
+import { routes } from './routes'
 
 export const Router = memo(() => {
   const {
@@ -23,29 +15,13 @@ export const Router = memo(() => {
   return (
     <BrowserRouter>
       <AdminLayout>
-        <Suspense fallback={<div></div>}>
+        <Suspense fallback={<Skeleton />}>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/authentication/login" component={Login} />
-            <Route exact path="/authentication/password" component={Password} />
-            <Route exact path="/authentication/register" component={Register} />
-            <Route exact path="/admin/patients" component={Patients} />
-            <Route exact path="/admin/patients/add" component={PatientForm} />
-            <Route
-              exact
-              path="/admin/patients/interactions/:id"
-              component={PatientInteractions}
-            />
-            <Route
-              exact
-              path="/admin/patients/assets/:id"
-              component={PatientAssets}
-            />
-            {/* {routes.map((route, index) => (
+            {routes.map((route, index) => (
               <Route key={index} {...route}>
                 <Page route={route} />
               </Route>
-            ))} */}
+            ))}
           </Switch>
         </Suspense>
       </AdminLayout>
