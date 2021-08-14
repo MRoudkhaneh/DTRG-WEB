38ebcecedd3eeb577aa4e91408f389c7526ab641
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-08-14 14:54:37 +0430

    DRAWER: Implement unit test for drawer

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -1,16 +1,10 @@
 import { FC, memo } from 'react'
-import { classNames } from 'utils/classes'
+import { drawerClassName } from './helper'
 
 export const Drawer: FC<IDrawer> = memo(({ children, open }) => {
   return (
-    <div
-      className={classNames(
-        'fixed top-0 left-0 h-full flex-col shadow-lg trans z-30 bg-gray-300 dark:bg-gray-900 ',
-        open ? ' w-72' : 'w-20 '
-      )}
-      slot="wrapper"
-    >
-      {children && <div slot="children">{children}</div>}
+    <div className={drawerClassName(open)} data-testid="drawer">
+      {children && <div>{children}</div>}
     </div>
   )
 })
