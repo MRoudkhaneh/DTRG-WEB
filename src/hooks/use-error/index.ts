37291e3779cfcc5239b26ef5d9f69c1574dddb0f
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-09-27 18:04:36 +0330

    ERROR: Implement error message for authentication

--- src/hooks/use-error/index.ts
+++ src/hooks/use-error/index.ts
@@ -1,23 +1,15 @@
 import { useCallback } from 'react'
-import { useHistory } from 'react-router'
 import { useToast } from 'hooks'
 
 export const useError = () => {
   const { error: errorToast } = useToast()
-  const { push } = useHistory()
 
   return {
     onError: useCallback((error) => {
-      if (error.message) {
-        switch (error.message) {
-          case 'Request failed with status code 401':
-            errorToast(error.message)
-            push('/authentication/login')
-            break
-          default:
-            errorToast(error.message)
-            break
-        }
+      if (error.response && error.response.data) {
+        errorToast(error.response.data.detail)
+      } else if (error.message) {
+        errorToast(error.message)
       }
     }, []),
   }
