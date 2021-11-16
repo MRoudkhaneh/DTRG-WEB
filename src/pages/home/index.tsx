36900d5fa054666665343291b21e695d9f80733c
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-16 20:53:28 +0330

    HOME: Add replace to home page redirect

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom'
 
 const View = () => {
-  return <Navigate to="/admin/patients" />
+  return <Navigate replace to="/admin/patients" />
 }
 
 export default View
