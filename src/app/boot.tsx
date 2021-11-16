3593c80a7eda63c5dfca86f789734f73a360f034
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-16 22:05:18 +0330

    BOOT: Memoise client for react query

--- src/app/boot.tsx
+++ src/app/boot.tsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { UiProvider } from 'provider/ui-provider'
 import { Router } from './router'
+import { useMemo } from 'react'
 
-const Boot = () => (
-  <QueryClientProvider client={new QueryClient()}>
-    <UiProvider>
-      <Router />
-    </UiProvider>
-  </QueryClientProvider>
-)
+const Boot = () => {
+  const client = useMemo(() => new QueryClient(), [])
+  return (
+    <QueryClientProvider client={client}>
+      <UiProvider>
+        <Router />
+      </UiProvider>
+    </QueryClientProvider>
+  )
+}
 
 export { Boot }
