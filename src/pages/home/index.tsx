43c5c4f7ad3b4aea978f36e209868cdb4796f80a
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-16 20:37:23 +0330

    HOME: Implement navigate component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 const View = () => {
-  const push = useNavigate()
-  useEffect(() => {
-    push('/admin/patients')
-  }, [])
-
-  return <div className="w-full"></div>
+  return <Navigate to="/admin/patients" />
 }
 
 export default View
