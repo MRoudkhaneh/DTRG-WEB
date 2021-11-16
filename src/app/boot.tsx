e8abac54f0e53b5302ff0a60280b59f106165b48
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-16 21:04:47 +0330

    BOOT: Fix toast re render issue

--- src/app/boot.tsx
+++ src/app/boot.tsx
@@ -1,17 +1,11 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { UiProvider } from 'provider/ui-provider'
 import { Router } from './router'
-import { BrowserRouter } from 'react-router-dom'
-import { AdminLayout } from 'layouts'
 
 const Boot = () => (
   <QueryClientProvider client={new QueryClient()}>
     <UiProvider>
-      <BrowserRouter>
-        <AdminLayout>
-          <Router />
-        </AdminLayout>
-      </BrowserRouter>
+      <Router />
     </UiProvider>
   </QueryClientProvider>
 )
--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,10 +1,11 @@
 import { memo, Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { Toast } from 'components/toast'
 import { Skeleton } from 'components/skeleton'
 import { useUi } from 'hooks/use-ui'
 import { routes } from './routes'
 import { NotFound } from './not-found'
+import { AdminLayout } from 'layouts'
 
 export const Router = memo(() => {
   const {
@@ -12,14 +13,18 @@ export const Router = memo(() => {
   } = useUi()
 
   return (
-    <Suspense fallback={<Skeleton />}>
-      <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} {...route} />
-        ))}
-        <Route element={<NotFound />} />
-      </Routes>
-      {toast.open && <Toast {...toast} />}
-    </Suspense>
+    <BrowserRouter>
+      <AdminLayout>
+        <Suspense fallback={<Skeleton />}>
+          <Routes>
+            {routes.map((route) => (
+              <Route key={route.path} {...route} />
+            ))}
+            <Route element={<NotFound />} />
+          </Routes>
+        </Suspense>
+        {toast.open && <Toast {...toast} />}
+      </AdminLayout>
+    </BrowserRouter>
   )
 })
