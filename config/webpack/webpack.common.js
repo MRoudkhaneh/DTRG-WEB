01a9b9d05565560c279307ad99def744a7d5aa1b
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-20 12:48:22 +0330

    UPGRADE: Upgrade dependencies

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -68,5 +68,5 @@ module.exports = {
     }),
   ],
 
-  stats: 'errors-only',
+  stats: 'minimal',
 }
--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -4,18 +4,15 @@ const webpack = require('webpack')
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
-
   devServer: {
     port: '8001',
     open: true,
     historyApiFallback: true,
-    hot: true,
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.variable': JSON.stringify('dev'),
     }),
     new ReactRefreshWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
   ],
 }
