dca6f5440e0046666e74588f13bf6296dc87e334
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-29 11:17:52 +0330

    FOLDER_STRUCTURE: Fix button folder structure

--- src/components/button/stories/button.stories.tsx
+++ src/components/button/stories/button.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Button } from 'components/button'
+import { ICDelete } from 'icons'
 
 export default {
   title: 'Components/Button',
@@ -16,19 +17,17 @@ Submit.args = {
     'w-1/4 my-10 mx-auto h-12 text-white shadow-blue bg-secondary dark:bg-primary',
 }
 
-// export const Secondary = Template.bind({})
-// Secondary.args = {
-//   label: 'Button',
-// }
+export const Cancel = Template.bind({})
 
-// export const Large = Template.bind({})
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// }
+Cancel.args = {
+  children: 'Cancel',
+  className: 'h-12  w-1/4  bg-gray-200 mx-auto my-10',
+}
 
-// export const Small = Template.bind({})
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// }
+export const Icon = Template.bind({})
+
+Icon.args = {
+  children: <ICDelete className="w-6 h-6 text-red-600" />,
+  className: ' mx-auto my-10',
+  icon: true,
+}
