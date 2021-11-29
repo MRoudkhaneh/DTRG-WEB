25e50ea7e34eadf4f1ac34b6f32a6b086a5b3278
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-11-29 18:07:43 +0330

    Confirm: Implement confirm test and stories

--- src/components/confirm/types/confirm.d.ts
+++ src/components/confirm/types/confirm.d.ts
@@ -1,9 +1,9 @@
 interface IConfirm {
   title: string
   description: string
-  type: 'delete' | 'warning'
+
   loading: boolean
-  dark: boolean
+
   onCancel: () => void
   onConfirm: () => void
 }
