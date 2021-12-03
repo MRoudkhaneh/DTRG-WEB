b4fd2768bfad15d888a575262ce533b5ee0c133a
Amir hossein khademi <amirhossein.khademi@connect2wow.com>
2021-12-03 11:54:16 +0330

    ERROR: Implement error story and test

--- src/components/error/index.tsx
+++ src/components/error/index.tsx
@@ -1,7 +1,6 @@
-import { memo } from 'react'
 import { classNames } from 'utils'
 
-export const Error = memo(({ error, className }: IError) => {
+export const Error = ({ error, className }: IError) => {
   if (error && error.message)
     return (
       <p
@@ -9,10 +8,9 @@ export const Error = memo(({ error, className }: IError) => {
           ' text-xs mt-2 text-red-700 dark:text-red-500 ',
           className
         )}
-        slot="wrapper"
       >
         {error.message}
       </p>
     )
   else return null
-})
+}
